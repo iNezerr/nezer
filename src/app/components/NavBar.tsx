a0745498186ee7634abf4eaf92722d88f873bb43
Eben <bonuzx1@gmail.com>
2024-10-17 05:38:08 +0000

    chore: Update footer and navbar logos

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -35,7 +35,7 @@ const Navbar: React.FC = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
+          EBEN.
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
