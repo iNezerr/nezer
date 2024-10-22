2564e8ac71eb8bc5ca780de2d381a2a37fdcf907
Eben <bonuzx1@gmail.com>
2024-10-22 20:05:31 +0100

    chore: Update heading in ProjectsSection component

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -71,7 +71,7 @@ const ProjectsSection: React.FC = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+        Some of my Projects
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
