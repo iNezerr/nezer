42782cf6fcaaa745022649fa60231e9bbcaa2e87
Eben <bonuzx1@gmail.com>
2024-10-22 19:57:00 +0100

    feat: Update project details and URLs in ProjectsSection component

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -17,58 +17,37 @@ interface Project {
 const projectsData: Project[] = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
+    title: "myCodeEdge Africa",
+    description: "MyCodeEdge is a platform I created to "+
+    "offer personalized tech coaching, helping learners "+
+    "master programming and advance their careers.",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://mycodedge.africa",
   },
   {
     id: 2,
-    title: "Photography Portfolio Website",
-    description: "Project 2 description",
+    title: "myChefAssit Kitchen Assistant",
+    description: "myChefAssist is a recipe recommendation app I built "+
+    "that suggests meals based on available ingredients, "+
+    "offering personalized cooking solutions.",
     image: "/images/projects/2.png",
     tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://mychefassist-fe.vercel.app/",
   },
   {
     id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
+    title: "Zenova Oil",
+    description: "Zenova Oil is a website I built for a diesel "+
+    "distribution company in Nigeria, designed to "+
+    "showcase their services and streamline customer orders for efficient fuel delivery.",
     image: "/images/projects/3.png",
     tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
+    previewUrl: "https://www.zenovaoil.com.ng/",
+  }
 ];
 
 const ProjectsSection: React.FC = () => {
@@ -104,11 +83,6 @@ const ProjectsSection: React.FC = () => {
           onClick={handleTagChange}
           name="Web"
           isSelected={tag === "Web"}
-        />
-        <ProjectTag
-          onClick={handleTagChange}
-          name="Mobile"
-          isSelected={tag === "Mobile"}
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
